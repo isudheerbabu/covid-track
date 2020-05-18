d3b71f242eb51ff3fcf6b3bdd49346c6b9129585
Sudheer <Sudheer.babu@gmail.com>
2020-05-18 20:21:43 +0530

    second initialization

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/covid19-feed'));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
 
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/covid19-feed/index.html'));
 });
 
 console.log('Server Connected!');
