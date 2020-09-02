cde00139c652636b2a9438ca23b9ba08aa56817c
Sudheer <Sudheer.babu@gmail.com>
2020-09-02 22:32:36 +0530

    2nd deploy commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist/covid19-feed'));
+app.use(express.static(__dirname + '/dist/covid-track'));
 
 app.listen(process.env.PORT || 8080);
 
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/covid19-feed/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/covid-track/index.html'));
 });
 
 console.log('Server Connected!');
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'covid19-feed'`, () => {
+  it(`should have as title 'covid-track'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('covid19-feed');
+    expect(app.title).toEqual('covid-track');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('covid19-feed app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('covid-track app is running!');
   });
 });
